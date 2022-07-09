aa2f77f6ad00a25b5eb62908b49a73d6b0eb58c0
ulfatun_amanah <ulfaamana@gmail.com>
2022-07-09 15:01:58 +0700

    created home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Kulineran</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/" >Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/foods" >Foods</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/keranjang">
+              Keranjang
+              <img src="https://img.icons8.com/external-pixer-icons-pack-dmitry-mirolyubov/44/000000/external-cart-retail-pixer-icons-pack-dmitry-mirolyubov.png"/>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/FoodsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <h3>Foods</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'FoodsView',
+  components: {
+    Navbar
+  }
+}
+</script>
