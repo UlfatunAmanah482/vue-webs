863d44c7bc32b509f16a3fac6a6751b113d912d2
ulfatun_amanah <ulfaamana@gmail.com>
2022-07-11 20:54:08 +0700

    finished food detail page

--- src/views/FoodDetailView.vue
+++ src/views/FoodDetailView.vue
@@ -19,17 +19,64 @@
           </nav>
         </div>
       </div>
+
+      <div class="row mt-3">
+        <div class="col-md-6">
+          <img :src=" '../assets/images/' + product.gambar " alt="..." class="img-fluid shadow" >
+        </div>
+        <div class="col-md-6">
+          <h2>
+            <strong>{{ product.nama }}</strong>
+          </h2>
+          <hr>
+          <h4>Harga: <strong>{{ product.harga}}</strong></h4>
+          <form class="mt-4">
+            <div class="form-group mb-3">
+              <label for="jumlah_pesanan" class="form-label">Jumlah Pesanan</label>
+              <input type="number" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+              <label for="keterangan" class="form-label">Keterangan</label>
+              <textarea class="form-control" placeholder="Keterangan misal: pedas, nasi setengah, dll"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">
+              <img src="https://img.icons8.com/material-outlined/24/FFFFFF/shopping-cart--v1.png"/>
+              Pesan
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      product: {}
+    }
+  },
+  methods: {
+    setProduct(data){
+      this.product = data;
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/products/"+this.$route.params.id)
+      .then((res) => {
+        console.log(res.data);
+        this.setProduct(res.data)
+      })
+      .catch((err) => console.log(err)) 
   },
 };
 </script>
