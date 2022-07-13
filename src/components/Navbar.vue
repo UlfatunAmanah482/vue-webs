c18bf309bc2e1233cb578c2805c1e56a9c5c4534
ulfatun_amanah <ulfaamana@gmail.com>
2022-07-13 14:08:47 +0700

    created cart page and connected with API

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,16 +21,9 @@
               <!-- Keranjang -->
               <img src="https://img.icons8.com/external-pixer-icons-pack-dmitry-mirolyubov/44/000000/external-cart-retail-pixer-icons-pack-dmitry-mirolyubov.png"/>
               <span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger">
-                {{ totalOrder.length }}
+                {{ updateCart ? updateCart.length : totalOrder.length }}
               </span>
             </router-link>
-            <!-- <button type="button" class="btn btn-primary position-relative">
-              Inbox
-              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                99+
-                <span class="visually-hidden">unread messages</span>
-              </span>
-            </button> -->
           </li>
         </ul>
       </div>
@@ -53,6 +46,7 @@ export default {
       this.totalOrder = data;
     }
   },
+  props: ["updateCart"],
   mounted() {
     axios
       .get("http://localhost:3000/carts")
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,22 +1,129 @@
 <template>
   <section>
-    <Navbar />
-    <h2>Cart</h2>
+    <Navbar :updateCart="carts" />
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark text-decoration-none"
+                  >Home</router-link
+                >
+              </li>
+              <li class="breadcrumb-item">
+                <router-link to="/foods" class="text-dark text-decoration-none"
+                  >Foods</router-link
+                >
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">Cart</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr align="center">
+                <th scope="col">No</th>
+                <th scope="col">Foto</th>
+                <th scope="col">Makanan</th>
+                <th scope="col">Keterangan</th>
+                <th scope="col">Jumlah</th>
+                <th scope="col">Harga</th>
+                <th scope="col">Total Harga</th>
+                <th scope="col">Hapus</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(cart, index) in carts" :key="cart.id" align="center">
+                <th>{{ index + 1 }}</th>
+                <td>
+                  <img
+                    :src="'../assets/images/' + cart.products.gambar"
+                    alt="..."
+                    class="img-fluid shadow"
+                    width="250"
+                  />
+                </td>
+                <td>
+                  <strong>{{ cart.products.nama }}</strong>
+                </td>
+                <td>{{ cart.keterangan ? cart.keterangan : "-" }}</td>
+                <td>{{ cart.jumlah_pemesanan }}</td>
+                <td>Rp. {{ cart.products.harga }}</td>
+                <td>
+                  <strong>Rp. {{ cart.products.harga * cart.jumlah_pemesanan }}</strong>
+                </td>
+                <td>
+                  <img src="https://img.icons8.com/ios-glyphs/30/E74C3C/trash--v1.png" @click="deleteItem(cart.id)" style="cursor: pointer" />
+                </td>
+              </tr>
+
+              <tr>
+                <td colspan="6"  align="right">
+                  <strong>Total Harga:</strong>
+                </td>
+                <td align="center">
+                  <strong>Rp. {{ totalHarga }}</strong>
+                </td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   name: "CartView",
   components: {
     Navbar,
   },
-}
+  data() {
+    return {
+      carts: []
+    }
+  },
+  methods: {
+    setCarts(data){
+      this.carts = data;
+    },
+    deleteItem(id){
+      axios
+        .delete("http://localhost:3000/carts/"+id)
+        .then((res) => console.log("success delete item"))
+        .catch((err) => console.log(err))
+
+      axios
+        .get("http://localhost:3000/carts")
+        .then((res) => this.setCarts(res.data))
+        .catch((err) => console.log(err));
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/carts")
+      .then((res) => this.setCarts(res.data))
+      .catch((err) => console.log(err));
+  },
+  computed: {
+    totalHarga(){
+      return this.carts.reduce((items, data) => {
+        return items + data.products.harga * data.jumlah_pemesanan
+      }, 0)
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
